ddf9091f14c7ea2d01bff97dcf76b7b4044e0e1a
André Canton <andrecanton18@gmail.com>
2021-09-09 16:31:58 -0300

    dark-mode

--- assets/js/modules/darkMode.js
+++ assets/js/modules/darkMode.js
@@ -1,7 +1,7 @@
 // =============== DARK LIGHT THEME =================
 const themeButton = document.querySelector('#theme-button')
 const darkTheme = 'dark-theme'
-const iconTheme = 'far fa-lightbulb'
+const iconTheme = 'dark'
 
 // Previously selecterd topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
@@ -9,12 +9,12 @@ const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'light' : 'dark'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+    themeButton.classList[selectedIcon === 'light' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with de button
@@ -23,6 +23,6 @@ themeButton.addEventListener('click', () => {
     themeButton.classList.toggle(iconTheme)
 
     // We save the theme and current icon that user chose
-     ge.setItem('selected-theme', getCurrentTheme())
+     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
